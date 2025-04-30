1951052e05d47a06f27e4ddcef19c68f414c0fb1
Manjot Kaur <manjotk2003@gmail.com>
2025-04-30 15:21:45 -0400

    connected contentview and newtodoview

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
     var body: some View {
         VStack {
             HStack {
@@ -16,7 +17,7 @@ struct ContentView: View {
                     .fontWeight(.black)
                 Spacer()
                 Button {
-                    
+                    showNewTask = true
                 } label: {
                     Text("+")
                         .font(.title)
@@ -25,6 +26,9 @@ struct ContentView: View {
             }
             .padding()
             Spacer()
+        }
+        if showNewTask {
+            NewToDoView()
         }
     }
 }

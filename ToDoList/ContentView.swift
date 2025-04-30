b77d2f72694ce564f6afee3ab879eeb44bc8a8c6
Manjot Kaur <manjotk2003@gmail.com>
2025-04-30 17:38:57 -0400

    complete delete functionality

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftData
 struct ContentView: View {
     @State private var showNewTask = false
     @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
+    
     var body: some View {
         VStack {
             HStack {
@@ -38,11 +40,19 @@ struct ContentView: View {
                         Text(toDoItem.title)
                     }
                 }
+                .onDelete(perform: deleteToDo)
             }
             .listStyle(.plain)
         }
         if showNewTask {
             NewToDoView(showNewTask: $showNewTask, toDoItem: ToDoItem(title: "", isImportant: false))
+        }
+    }
+    
+    func deleteToDo(at offsets: IndexSet) {
+        for offset in offsets {
+            let toDoItem = toDos[offset]
+            modelContext.delete(toDoItem)
         }
     }
 }
